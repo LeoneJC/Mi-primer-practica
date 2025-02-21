e6c7dc4a0b6664b9ef26fa6023d4e7568dde81b9
Juan Cruz L <leonecruzjuan@gmail.com>
2025-02-21 18:21:38 -0300

    Probando si me sube el archivo .py

---
+++ numeros de mayor a menor.py
@@ -0,0 +1,8 @@
+a=int(input("Introduce el primer numero: "))
+b=int(input("Introduce el segundo numero: "))
+
+if a>b:
+    print(f"El numero {a} es mayor a {b}")
+else:
+    print(f"El numero {b} es mayor a {a}")
+
